f7873ff7957bf3962fe202cd1fbe880655257eaf
Allen Tripp <trippa002@gmail.com>
2026-02-02 23:58:33 -0600

    exercises completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let _word = '';
+    if (times < 0) return 'ERROR'
+    else {
+        for (let i = 0; i < times; i++) {
+            _word += word;            
+        }
+        return _word;
+    }
 };
 
 // Do not edit below this line
